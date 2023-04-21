b435f64f3e842f52f0dd706272965cd9f5621c56
Thibault Delattre <thibo.delattre@gmail.com>
2023-04-21 16:37:06 +0200

    add example

---
+++ examples/get_address.rs
@@ -0,0 +1,18 @@
+use rusb2snes::SyncClient;
+
+fn main() {
+    let mut usb2snes = SyncClient::connect().unwrap();
+
+
+    usb2snes.set_name(String::from("Snes Input Viewer")).unwrap();
+
+    let devices = usb2snes.list_device().unwrap();
+
+    usb2snes.attach(&devices[0]).unwrap();
+    let info = usb2snes.info().unwrap();
+    println!("Attached to {} - {}", info.dev_type, info.version);
+    let address = usb2snes.get_address(0xF5008B, 2).unwrap();
+    for elt in address {
+        dbg!(elt);
+    }
+}
